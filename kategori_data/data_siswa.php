85b068289ca48d87ac48ba5b3222802d69598a7c
brianwibowo <sultanapri86@gmail.com>
2025-04-19 23:25:13 +0700

    sssssss

--- kategori_data/data_siswa.php
+++ kategori_data/data_siswa.php
@@ -32,7 +32,7 @@
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Beranda</h1>
+                    <h1 class="mt-4">Data Siswa</h1>
 
                     <div class="card mb-4">
                         <div class="card-header">
