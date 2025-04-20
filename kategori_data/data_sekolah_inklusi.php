09aaf24376d8d2bfffc83e65ad3149734f63f77c
Fluxzz <anugrahrdho14@gmail.com>
2025-04-20 22:31:02 +0700

    dikit

--- kategori_data/data_sekolah_inklusi.php
+++ kategori_data/data_sekolah_inklusi.php
@@ -1,19 +1,27 @@
-<?php include '../partials/head.php'; ?>
+<?php 
+include '../partials/head.php'; 
+include '../koneksi.php';
+?>
+
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand ps-3" href="index.php"><img src="/logo_dinas.png" alt="Logo" width="50" height="40">SILANDIK</a>
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i class="fas fa-bars"></i></button>
+        <a class="navbar-brand ps-3" href="index.php">
+            <img src="/logo_dinas.png" alt="Logo" width="50" height="40"> SILANDIK
+        </a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle">
+            <i class="fas fa-bars"></i>
+        </button>
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         </form>
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown">
+                    <i class="fas fa-user fa-fw"></i>
+                </a>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="/authentification/login.php">Login</a></li>
                     <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
+                    <li><hr class="dropdown-divider" /></li>
                     <li><a class="dropdown-item" href="#">Logout</a></li>
                 </ul>
             </li>
@@ -21,71 +29,86 @@
     </nav>
 
     <div id="layoutSidenav">
-    <?php include '../sidebar.php'; ?>
+        <?php include '../sidebar.php'; ?>
 
-    <div id="layoutSidenav_content">
-        <main>
-            <div class="container-fluid px-4">
-                <h1 class="mt-4">Data Sekolah Inklusi</h1>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Data Sekolah Inklusi</h1>
 
-
-                <div class="card mb-4">
-                    <div class="card-header d-flex justify-content-between align-items-center">
-                        <div>
-                            <i class="fas fa-table me-1"></i>
+                    <div class="card mb-4">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <div>
+                                <i class="fas fa-table me-1"></i>
                                 Daftar Sekolah Inklusi
-                        </div>
-                            <a href="tambah_sekolah.php" class="btn btn-success btn-sm">
+                            </div>
+                            <a href="tambah_sekolah_inklusi.php" class="btn btn-success btn-sm">
                                 <i class="fas fa-plus"></i> 
                                 Tambah Data Sekolah
                             </a>
+                        </div>
+
+                        <div class="card-body">
+                            <table id="datatablesSimple" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Logo</th>
+                                        <th>Nama Sekolah</th>
+                                        <th>Deskripsi</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Logo</th>
+                                        <th>Nama Sekolah</th>
+                                        <th>Deskripsi</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <?php
+                                    $no = 1;
+                                    $query = mysqli_query($conn, "SELECT * FROM data_sekolah_inklusi");
+
+                                    if (!$query) {
+                                        die("Query error: " . mysqli_error($conn));
+                                    }
+
+                                    while ($row = mysqli_fetch_assoc($query)) {
+                                    ?>
+                                        <tr>
+                                            <td><?= $no++; ?></td>
+                                            <td>
+                                                <img src="../upload/<?= htmlspecialchars($row['logo_sekolah']); ?>" alt="Logo" width="50">
+                                            </td>
+                                            <td><?= htmlspecialchars($row['nama_sekolah']); ?></td>
+                                            <td><?= htmlspecialchars($row['deskripsi']); ?></td>
+                                            <td>
+                                                <a href="edit_sekolah_inklusi.php?id=<?= htmlspecialchars($row['id']); ?>" 
+                                                    class="btn btn-warning btn-sm" 
+                                                    onclick="return confirm('Yakin ingin mengubah data ini?')">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <a href="delete_sekolah_inklusi.php?id=<?= htmlspecialchars($row['id']); ?>" 
+                                                    class="btn btn-danger btn-sm" 
+                                                    onclick="return confirm('Yakin ingin menghapus data ini?')">
+                                                    <i class="fas fa-trash"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
 
-                    <div class="card-body">
-                        <table id="datatablesSimple" class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Logo</th>
-                                    <th>Nama Sekolah</th>
-                                    <th>Deskripsi</th>
-                                    <th>Kecamatan</th>
-                                    <th>Status</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tfoot>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Logo</th>
-                                    <th>Nama Sekolah</th>
-                                    <th>Deskripsi</th>
-                                    <th>Kecamatan</th>
-                                    <th>Jenjang</th>
-                                    <th>Status</th>
-                                </tr>
-                            </tfoot>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td><img src="/logo_dinas.png" alt="Logo" style="width: 60px; height: auto; border-radius: 5px;"></td>
-                                    <td>SMP Harapan Bangsa</td>
-                                    <td>SMP inklusi yang membuka kesempatan untuk peserta didik berkebutuhan khusus.</td>
-                                    <td>Semarang Timur</td>
-                                    <td>Negeri</td>
-                                    <td>
-                                        <a href="edit_sekolah.php?id=2" class="btn btn-sm btn-primary">Edit</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
-
-            </div>
-        </main>
+            </main>
+        </div>
     </div>
-</div>
 
     <?php include '../partials/footer.php'; ?>
 </body>
--- kategori_data/tambah_sekolah_inklusi.php
+++ kategori_data/tambah_sekolah_inklusi.php
@@ -1,17 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+include '../koneksi.php';
+include '../partials/head.php';
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Login - SB Admin</title>
-    <link href="/css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama_sekolah = htmlspecialchars($_POST['nama_sekolah']);
+    $deskripsi = htmlspecialchars($_POST['deskripsi']);
 
-</head>
+    // Handle upload logo
+    $targetDir = __DIR__ . '/../upload/';
+    $fileName = basename($_FILES["logo"]["name"]);
+    $targetFilePath = $targetDir . $fileName;  
+    $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+
+    // Validasi file gambar
+    $allowTypes = ['jpg', 'jpeg', 'png',];
+    if (in_array($fileType, $allowTypes)) {
+        if (move_uploaded_file($_FILES["logo"]["tmp_name"], $targetFilePath)) {
+            // Simpan data ke database
+            $query = "INSERT INTO data_sekolah_inklusi (nama_sekolah, logo_sekolah, deskripsi) 
+                      VALUES ('$nama_sekolah', '$fileName', '$deskripsi')";
+            mysqli_query($conn, $query);
+
+            header("Location: data_sekolah_inklusi.php");
+            exit();
+        } else {
+            $error = "Gagal mengunggah logo sekolah.";
+        }
+    } else {
+        $error = "Format file logo tidak didukung. Gunakan JPG, JPEG, PNG, atau GIF.";
+    }
+}
+?>
 
 <body class="bg-primary">
     <div id="layoutAuthentication">
@@ -20,21 +39,28 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-5">
-                        <div class="container mt-5">
+                            <div class="container mt-5">
                                 <div class="card">
                                     <div class="card-header">
                                         <h4>Form Tambah Data Sekolah Inklusi</h4>
                                     </div>
                                     <div class="card-body">
-                                        <form action="proses_tambah.php" method="POST" enctype="multipart/form-data">
+                                        <?php if (isset($error)): ?>
+                                            <div class="alert alert-danger"><?= $error; ?></div>
+                                        <?php endif; ?>
+                                        <form method="POST" enctype="multipart/form-data">
                                             <div class="mb-3">
-                                                <label for="nama" class="form-label">Nama Sekolah</label>
-                                                <input type="text" name="nama" class="form-control" id="nama" required>
+                                                <label for="nama_sekolah" class="form-label">Nama Sekolah</label>
+                                                <input type="text" name="nama_sekolah" class="form-control" id="nama_sekolah" required>
                                             </div>
 
                                             <div class="mb-3">
                                                 <label for="logo" class="form-label">Logo Sekolah</label>
-                                                <input type="file" name="logo" class="form-control" id="logo" accept="image/*">
+                                                <input type="file" name="logo" class="form-control" id="logo" accept="image/*" onchange="previewImage(event)" required>
+                                                <div id="logoPreviewContainer" class="mt-3" style="display: none;">
+                                                    <h5>Preview Logo:</h5>
+                                                    <img id="logoPreview" src="" alt="Logo Preview" style="width: 150px; height: auto; border-radius: 5px;">
+                                                </div>
                                             </div>
 
                                             <div class="mb-3">
@@ -42,35 +68,8 @@
                                                 <textarea name="deskripsi" class="form-control" id="deskripsi" rows="4" required></textarea>
                                             </div>
 
-                                            <div class="mb-3">
-                                                <label for="kecamatan" class="form-label">Kecamatan</label>
-                                                <input type="text" name="kecamatan" class="form-control" id="kecamatan">
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <label for="jenjang" class="form-label">Jenjang Sekolah</label>
-                                                <select name="jenjang" id="jenjang" class="form-select">
-                                                    <option value="">-- Pilih Jenjang --</option>
-                                                    <option value="TK">TK</option>
-                                                    <option value="SD">SD</option>
-                                                    <option value="SMP">SMP</option>
-                                                    <option value="SMA">SMA</option>
-                                                    <option value="SMK">SMK</option>
-                                                </select>
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <label for="status" class="form-label">Status</label>
-                                                <select name="status" id="status" class="form-select">
-                                                    <option value="">-- Pilih Status --</option>
-                                                    <option value="Negeri">Negeri</option>
-                                                    <option value="Swasta">Swasta</option>
-                                                </select>
-                                            </div>
-
-                                            <button type="submit" class="btn btn-success">
-                                                <i class="fas fa-save"></i> Simpan Data
-                                            </button>
+                                            <button type="submit" class="btn btn-success">Simpan</button>
+                                            <a href="data_sekolah_inklusi.php" class="btn btn-secondary">Kembali</a>
                                         </form>
                                     </div>
                                 </div>
@@ -81,7 +80,20 @@
             </main>
         </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="/js/scripts.js"></script>
+    <script>
+    function previewImage(event) {
+        const reader = new FileReader();
+        reader.onload = function() {
+            const output = document.getElementById('logoPreview');
+            const container = document.getElementById('logoPreviewContainer');
+            output.src = reader.result;
+            container.style.display = 'block';
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+    </script>
 </body>
 </html>
